c2326a2af9390829e6ec880fb7082938df4d2f5e
silverywaves <babyshu1210@gmail.com>
2024-05-27 17:40:20 +0900

    SerialPortConn

---
+++ LEC/C#/02_SERIAL_PORT_CONN/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8"});
+            this.comboBox1.Location = new System.Drawing.Point(37, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(230, 32);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "연결";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 115);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(380, 277);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ LEC/C#/02_SERIAL_PORT_CONN/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 시리얼 데이터 수신 이벤트 처리 함수
+        private void SerialPort_DataReceived(Object sender, SerialDataReceivedEventArgs e)
+        {
+            String recvData = this.serialPort.ReadExisting();
+            Console.WriteLine(recvData);
+
+            this.Invoke(
+                (MethodInvoker)delegate
+                {
+                    this.textBox1.AppendText(recvData + "\r\n");
+                }
+            );
+
+            Thread.Sleep(1000);
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("연결 Btn Clicked..");
+            String port_number = null;
+
+            try {
+                if (this.comboBox1.SelectedIndex > -1){
+                    port_number = this.comboBox1.Items[this.comboBox1.SelectedIndex].ToString();
+                    Console.WriteLine("Selected Port : " + port_number);
+
+                    // SERIAL PORT 연결
+                    this.serialPort.PortName = port_number;
+                    this.serialPort.BaudRate = 9600;
+                    this.serialPort.DataBits = 8;
+                    this.serialPort.StopBits = System.IO.Ports.StopBits.One;
+                    this.serialPort.Parity = System.IO.Ports.Parity.None;
+                    this.serialPort.Open();
+                    Console.WriteLine("CONNECTION SUCCESS..." + this.serialPort);
+                    this.serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Write("CONNECTION ERROR : " + ex);
+                this.serialPort.Close();
+            }
+        }
+    }
+}
